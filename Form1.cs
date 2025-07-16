cfee51d518069c5e4de95f19aa69d167d871e83e
ramtin2005 <153830246+ramtin2005@users.noreply.github.com>
2025-07-16 21:54:55 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "ramtin" && textBox2.Text == "1384")
+            {
+                //MessageBox.Show("خوش آمدید");
+
+                Form frm2 = new Form2();
+                this.Hide();
+                frm2.Show();
+            }
+            else
+            {
+                MessageBox.Show("نام کاربری یا رمز عبور اشتباه می باشد");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Close();
+
+            DialogResult q = MessageBox.Show("آیا می خواهید خارج شوید؟", "",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (q == DialogResult.Yes)
+                Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //if(checkBox1.Checked)
+            //    textBox2.PasswordChar = '\0';
+            //else
+            //    textBox2.PasswordChar = '*';
+
+            if (textBox2.PasswordChar == '*')
+                textBox2.PasswordChar = '\0';
+            else
+                textBox2.PasswordChar = '*';
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace login
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "به صفحه اصلی خوش آمدید";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(335, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "خروج";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
